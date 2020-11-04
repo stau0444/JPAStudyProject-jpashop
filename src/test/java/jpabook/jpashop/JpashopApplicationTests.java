534f9c059ed7c8e2bfa148e7c19ca2e5eea5b9fc
stau04 <stau04@gmail.com>
2020-11-04 18:22:40 +0900

    JPAstudy 2020-11-04 project created

---
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test ok");
+	}
+
+}
